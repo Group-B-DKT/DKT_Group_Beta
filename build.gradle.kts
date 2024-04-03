3cf65bc72cf1839133a9e6566f7e65c4b3516a39
KevinSappl <sappl.kevke@gmx.at>
2024-04-03 12:49:52 +0200

    refresh sonarcube

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    id("org.sonarqube") version "4.4.1.3373"
+
     alias(libs.plugins.androidApplication) apply false
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "Group-B-DKT_DKT_Group_Beta")
+        property("sonar.organization", "group-b-dkt")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 }
