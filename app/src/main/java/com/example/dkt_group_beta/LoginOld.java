fd42537f9da955596037131700f234d6b6f8e579
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-04-04 16:57:50 +0200

    added spielverbindung

--- app/src/main/java/com/example/dkt_group_beta/LoginOld.java
+++ app/src/main/java/com/example/dkt_group_beta/LoginOld.java
@@ -11,24 +11,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login2 extends AppCompatActivity {
+public class LoginOld extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login2);
+        setContentView(R.layout.activity_login_old);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        Button loginButton2 = findViewById(R.id.LoginButton2);
-        loginButton2.setOnClickListener(new View.OnClickListener() {
+        Button loginButton = findViewById(R.id.LoginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login2.this, Spielverbindung.class);
+                Intent intent = new Intent(LoginOld.this, Spielverbindung.class);
                 startActivity(intent);
             }
         });
