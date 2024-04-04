19970477251a951d49309755fb54fb04c75150da
Tomasini Hannah <hatomasini@edu.aau.at>
2024-04-04 17:18:51 +0200

    renamed activity_main.xml to activity_main2.xml

--- app/src/main/java/com/example/dkt_group_beta/MainActivity2.java
+++ app/src/main/java/com/example/dkt_group_beta/MainActivity2.java
@@ -19,7 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         Button button = findViewById(R.id.searchGames);
         games = findViewById(R.id.games);
