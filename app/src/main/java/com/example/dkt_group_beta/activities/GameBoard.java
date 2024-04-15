6ee38372b8458d482f1a152b996bcd7a63efee08
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-04-15 15:03:45 +0200

    added onClick() to open popup

--- app/src/main/java/com/example/dkt_group_beta/activities/GameBoard.java
+++ app/src/main/java/com/example/dkt_group_beta/activities/GameBoard.java
@@ -1,6 +1,11 @@
 package com.example.dkt_group_beta.activities;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +27,17 @@ public class GameBoard extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void my_button_onClick_working(View view) {
+        LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.activity_pop_up, null);
+
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
+        boolean focusable = true;
+        PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
     }
 }
