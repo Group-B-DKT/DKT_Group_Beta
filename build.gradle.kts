7772aec902c8d95ba5182fe42b757a4bd2bed286
KevinSappl <sappl.kevke@gmx.at>
2024-04-15 16:47:10 +0200

    Reinitialisation of Sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,11 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("org.sonarqube") version "4.4.1.3373"
-    id("jacoco")
-    alias(libs.plugins.androidApplication) apply false
 }
 
 sonar {
     properties {
-        property("sonar.gradle.skipCompile", "true")
         property("sonar.projectKey", "Group-B-DKT_DKT_Group_Beta")
         property("sonar.organization", "group-b-dkt")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${projectDir}/build/reports/jacoco/testDebugUnitTestCoverage/testDebugUnitTestCoverage.xml")
-        property("sonar.core.codeCoveragePlugin", "jacoco")
-    }
-}
-jacoco {
-    toolVersion = "0.8.11"
-}
-tasks.withType<JacocoReport> {
-    reports {
-        xml.required.set(true)
-        html.required.set(true)
     }
 }
