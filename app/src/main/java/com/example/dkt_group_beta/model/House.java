18ceced99ac2d7fc4a89445436d27d6564e5764e
Tomasini Hannah <hatomasini@edu.aau.at>
2024-04-18 12:13:04 +0200

    Extended building class to subclasses house and hotel

---
+++ app/src/main/java/com/example/dkt_group_beta/model/House.java
@@ -0,0 +1,24 @@
+package com.example.dkt_group_beta.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+public class House extends Building{
+    @Getter
+    private static final int HOUSE_PRICE = 200;
+    @Getter
+    private int MAX_AMOUNT = 4;
+
+    public House(int HOUSE_PRICE, Player owner, int position) {
+        super(HOUSE_PRICE, owner, position);
+    }
+
+    public int getHousePrice(){
+        return HOUSE_PRICE;
+    }
+
+    public int getMAX_AMOUNT(){
+        return MAX_AMOUNT;
+    }
+}
