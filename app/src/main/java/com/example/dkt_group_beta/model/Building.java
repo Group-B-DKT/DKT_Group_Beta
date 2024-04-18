4b0fe36a8b694b8c61f9046a85feca43d924604e
Tomasini Hannah <hatomasini@edu.aau.at>
2024-04-18 11:17:33 +0200

    Created building class

---
+++ app/src/main/java/com/example/dkt_group_beta/model/Building.java
@@ -0,0 +1,38 @@
+package com.example.dkt_group_beta.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+public abstract class Building {
+    @Getter
+    private int price;
+    @Setter
+    @Getter
+    private Player owner;
+    @Getter
+    private int position;
+
+    public Building (int price, Player owner, int position){
+        this.price = price;
+        this.owner = owner;
+        this.position = position;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public int getPosition(){
+        return  position;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+    public Player getOwner(){
+        return owner;
+    }
+}
