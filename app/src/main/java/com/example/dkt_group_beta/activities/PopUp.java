ad00c8ae00a46ae80d48d0e486f7767c85a8fe44
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-04-22 17:59:44 +0200

    conflict fixed

---
+++ app/src/main/java/com/example/dkt_group_beta/activities/PopUp.java
@@ -0,0 +1,37 @@
+package com.example.dkt_group_beta.activities;
+
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.dkt_group_beta.R;
+
+public class PopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pop_up);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        setContentView(R.layout.activity_pop_up);
+        getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int) (width*0.8), (int) (height*0.6));
+    }
+}
