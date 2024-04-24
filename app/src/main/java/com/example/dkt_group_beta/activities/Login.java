0abf9ab35e9cbbd31b5cff6896b5ed47d3d0a182
KevinSappl <sappl.kevke@gmx.at>
2024-04-24 18:06:28 +0200

    Removed test Code

--- app/src/main/java/com/example/dkt_group_beta/activities/Login.java
+++ app/src/main/java/com/example/dkt_group_beta/activities/Login.java
@@ -25,7 +25,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.d("debug", "Total sinnlos nur zum test");
 
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
