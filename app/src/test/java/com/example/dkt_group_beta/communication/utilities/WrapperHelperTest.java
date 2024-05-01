6c83d7be97f528dc178069a1ee8778f86a2efbba
KevinSappl <sappl.kevke@gmx.at>
2024-05-02 01:14:25 +0200

    Changed Dependenices to junit 5

--- app/src/test/java/com/example/dkt_group_beta/communication/utilities/WrapperHelperTest.java
+++ app/src/test/java/com/example/dkt_group_beta/communication/utilities/WrapperHelperTest.java
@@ -15,60 +15,53 @@ import com.example.dkt_group_beta.communication.enums.Info;
 import com.example.dkt_group_beta.communication.enums.Request;
 import com.google.gson.Gson;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class WrapperHelperTest {
     private static Gson gson;
 
-    @BeforeAll
-    public static void setUp(){
+    @BeforeEach
+    public void setUp(){
         System.out.println("ASD");
         gson = new Gson();
     }
-
     @Test
-    public void getInstanceFromWrapperReturnNull(){
+    public void testGetInstanceFromWrapperReturnNull(){
         assertNull(WrapperHelper.getInstanceFromWrapper(new Wrapper(null, -1, Request.INFO, null)));
     }
-
     @Test
-    public void getInstanceFromWrapperConnectJsonObject(){
+    public void testGetInstanceFromWrapperConnectJsonObject(){
         ConnectJsonObject connectJsonObject = new ConnectJsonObject(ConnectType.NEW_CONNECT);
         Wrapper wrapper = new Wrapper(connectJsonObject.getClass().getSimpleName(), -1, Request.CONNECT, connectJsonObject);
         assertTrue(WrapperHelper.getInstanceFromWrapper(wrapper) instanceof ConnectJsonObject);
     }
-
     @Test
-    public void getInstanceFromWrapperReturnActionJsonObject(){
+    public void testGetInstanceFromWrapperReturnActionJsonObject(){
         ActionJsonObject actionJsonObject = new ActionJsonObject(Action.ROLL_DICE);
         Wrapper wrapper = new Wrapper(actionJsonObject.getClass().getSimpleName(), -1, Request.CONNECT, actionJsonObject);
         assertTrue(WrapperHelper.getInstanceFromWrapper(wrapper) instanceof ActionJsonObject);
     }
-
     @Test
-    public void getInstanceFromWrapperReturnInfoJsonObject(){
+    public void testGetInstanceFromWrapperReturnInfoJsonObject(){
         InfoJsonObject infoJsonObject = new InfoJsonObject(Info.GAME_LIST);
         Wrapper wrapper = new Wrapper(infoJsonObject.getClass().getSimpleName(), -1, Request.CONNECT, infoJsonObject);
         assertTrue(WrapperHelper.getInstanceFromWrapper(wrapper) instanceof InfoJsonObject);
     }
-
     @Test
-    public void getInstanceFromJsonReturnTrue(){
+    public void testGetInstanceFromJsonReturnTrue(){
         InfoJsonObject infoJsonObject = new InfoJsonObject(Info.GAME_LIST);
         Wrapper wrapper = new Wrapper(infoJsonObject.getClass().getSimpleName(), -1, Request.CONNECT, infoJsonObject);
         String json = gson.toJson(wrapper);
         assertTrue(WrapperHelper.getInstanceFromJson(json) instanceof InfoJsonObject);
     }
-
     @Test
-    public void getInstanceFromJsonReturnNull(){
+    public void testGetInstanceFromJsonReturnNull(){
         String json = "";
         assertNull(WrapperHelper.getInstanceFromJson(json));
     }
-
     @Test
-    public void toJsonFromObjectCorrect(){
+    public void testToJsonFromObjectCorrect(){
         ConnectJsonObject connectJsonObject = new ConnectJsonObject(ConnectType.NEW_CONNECT);
         Wrapper wrapper = new Wrapper(connectJsonObject.getClass().getSimpleName(), -1, Request.CONNECT, connectJsonObject);
         String expected = gson.toJson(wrapper);
--- app/src/test/java/com/example/dkt_group_beta/viewmodel/LoginViewModelTest.java
+++ app/src/test/java/com/example/dkt_group_beta/viewmodel/LoginViewModelTest.java
@@ -1,43 +1,40 @@
 package com.example.dkt_group_beta.viewmodel;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import androidx.test.core.app.ApplicationProvider;
-
 import com.example.dkt_group_beta.activities.interfaces.LoginAction;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.RobolectricTestRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.robolectric.RuntimeEnvironment;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-@RunWith(RobolectricTestRunner.class)
+import org.robolectric.junit.jupiter.RobolectricExtension;
+
+@ExtendWith(RobolectricExtension.class)
 public class LoginViewModelTest {
     private Context context;
     private LoginAction loginAction;
     private LoginViewModel loginViewModel;
-
-
-    @Before
+    @BeforeEach
     public void setUp() {
 
-        context = ApplicationProvider.getApplicationContext();
+        context = RuntimeEnvironment.getApplication();
         loginViewModel = new LoginViewModel(context, loginAction);
     }
-
     @Test
     public void testgetSharedPreference() {
         try {
 
             LoginViewModel loginViewModel = new LoginViewModel(context, loginAction);
             SharedPreferences sharedPreferences = loginViewModel.getSharedPreference();
-            assertEquals(true, sharedPreferences != null);
+            assertNotNull(sharedPreferences);
         } catch (GeneralSecurityException | IOException e) {
             return;
         }
