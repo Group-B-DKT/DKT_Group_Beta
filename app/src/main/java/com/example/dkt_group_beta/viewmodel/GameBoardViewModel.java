7ebb7febe78f8ac6ac25c30a7355c2f7117fb8e1
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-05-03 20:31:37 +0200

    implemented handleAction() for Action roll dice

--- app/src/main/java/com/example/dkt_group_beta/viewmodel/GameBoardViewModel.java
+++ app/src/main/java/com/example/dkt_group_beta/viewmodel/GameBoardViewModel.java
@@ -2,22 +2,34 @@ package com.example.dkt_group_beta.viewmodel;
 
 import com.example.dkt_group_beta.activities.interfaces.GameBoardAction;
 import com.example.dkt_group_beta.communication.controller.ActionController;
+import com.example.dkt_group_beta.communication.controller.WebsocketClientController;
 import com.example.dkt_group_beta.communication.enums.Action;
 import com.example.dkt_group_beta.model.Game;
 import com.example.dkt_group_beta.model.Player;
+import com.google.gson.Gson;
 
 public class GameBoardViewModel {
     private ActionController actionController;
     private GameBoardAction gameBoardAction;
     private Game game;
+    private Player player;
 
 
     public GameBoardViewModel(GameBoardAction gameBoardAction, Game game) {
-        this.actionController = actionController;
+        this.actionController = new ActionController(this::handleAction);
         this.game = game;
+        player = WebsocketClientController.getPlayer();
     }
     void handleAction(Action action, String param, Player fromPlayer){
-
+        if(action == Action.ROLL_DICE) {
+            // array zurücksetzten, popup öffnen, showbothdice
+            if (fromPlayer.getId().equals(player.getId()) && !player.isHost()) {
+                return;
+            }
+            Gson gson = new Gson();
+            int[] diceResult = gson.fromJson(param,int[].class);
+            gameBoardAction.dicePopUp();
+        }
     }
 
     public int getRandomNumber(int min, int max){
