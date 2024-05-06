eb1a1c8504da702c88fff5abb2e6af3abc102bbe
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-05-06 15:04:12 +0200

    modified GameBoardViewModel constructor and handleAction()

--- app/src/main/java/com/example/dkt_group_beta/viewmodel/GameBoardViewModel.java
+++ app/src/main/java/com/example/dkt_group_beta/viewmodel/GameBoardViewModel.java
@@ -1,12 +1,18 @@
 package com.example.dkt_group_beta.viewmodel;
 
+import android.util.Log;
+
 import com.example.dkt_group_beta.activities.interfaces.GameBoardAction;
 import com.example.dkt_group_beta.communication.controller.ActionController;
 import com.example.dkt_group_beta.communication.controller.WebsocketClientController;
 import com.example.dkt_group_beta.communication.enums.Action;
+import com.example.dkt_group_beta.model.Field;
 import com.example.dkt_group_beta.model.Game;
 import com.example.dkt_group_beta.model.Player;
 import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GameBoardViewModel {
     private ActionController actionController;
@@ -17,18 +23,22 @@ public class GameBoardViewModel {
 
     public GameBoardViewModel(GameBoardAction gameBoardAction, Game game) {
         this.actionController = new ActionController(this::handleAction);
+        this.gameBoardAction = gameBoardAction;
         this.game = game;
         player = WebsocketClientController.getPlayer();
     }
-    void handleAction(Action action, String param, Player fromPlayer){
+    void handleAction(Action action, String param, Player fromPlayer, List<Field> fields){
         if(action == Action.ROLL_DICE) {
             // array zurücksetzten, popup öffnen, showbothdice
             if (fromPlayer.getId().equals(player.getId()) && !player.isHost()) {
                 return;
             }
+            Log.d("game",""+action);
             Gson gson = new Gson();
             int[] diceResult = gson.fromJson(param,int[].class);
+            Log.d("game",""+ Arrays.toString(diceResult));
             gameBoardAction.dicePopUp();
+            gameBoardAction.showBothDice(diceResult);
         }
     }
 
