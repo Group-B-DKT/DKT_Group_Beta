7ffcf470c1e542ce69a6f6d327948e83e5f7b25d
chriz133 <melchior133@gmail.com>
2024-05-10 00:05:12 +0200

    added test case for getOwnedFields method

--- app/src/test/java/com/example/dkt_group_beta/model/GameTest.java
+++ app/src/test/java/com/example/dkt_group_beta/model/GameTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -54,5 +55,17 @@ class GameTest {
         int max = 2;
         int result = game.getRandomNumber(min,max);
         assertTrue(min <= result && result <= max);
+    }
+
+    @Test
+    void testGetOwnedFields(){
+        Player player = new Player("User1", "ID1");
+        List<Field> fields1 = new ArrayList<>();
+        Field field = new Field(1, "Field1", true);
+        field.setOwner(player);
+        fields1.add(field);
+        fields1.add(new Field(2, "Field2", true));
+        Game game1 = new Game(Collections.singletonList(player), fields1);
+        assertEquals(1, game1.getOwnedFields(player).size());
     }
  }
