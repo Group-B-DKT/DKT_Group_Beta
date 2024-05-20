ba9464ebc8cca89434ba2153a732b33475d33a25
Sylvia Pisjak <sylviapi@edu.aau.at>
2024-05-20 20:41:56 +0200

    create card class

---
+++ app/src/main/java/com/example/dkt_group_beta/model/Card.java
@@ -0,0 +1,49 @@
+package com.example.dkt_group_beta.model;
+
+import com.example.dkt_group_beta.model.enums.CardType;
+
+public class Card {
+    private int id;
+    private int amount;
+    private String imageResource;
+    private CardType type;
+
+    public Card(int id, int amount, CardType type,String imageResource) {
+        this.id = id;
+        this.amount = amount;
+        this.imageResource = imageResource;
+        this.type = type;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setImageResource(String imageResource) {
+        this.imageResource = imageResource;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CardType getType() {
+        return type;
+    }
+
+    public void setType(CardType type) {
+        this.type = type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public String getImageResource() {
+        return imageResource;
+    }
+}
